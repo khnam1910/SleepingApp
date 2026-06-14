ff6c563e103062fb1a1d1d343d2e82364bcaf6c1
khnam1910 <khnam2003@gmail.com>
2026-06-14 21:18:34 +0700

    create login view

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:sleeping_app_flutter/presentation/auth/pages/login.dart';
+import 'core/theme/app_colors.dart';
+import 'core/theme/app_typography.dart';
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SleepingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SleepingApp extends StatelessWidget {
+  const SleepingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Organic Sleep',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: AppColors.lightColorScheme,
+        scaffoldBackgroundColor: AppColors.lightColorScheme.surface,
+        textTheme: AppTypography.lightTextTheme,
+      ),
+
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: AppColors.darkColorScheme,
+        scaffoldBackgroundColor: AppColors.darkColorScheme.surface,
+        textTheme: AppTypography.darkTextTheme,
+      ),
+
+      themeMode: ThemeMode.light,
+
+      home: LoginPage(),
+    );
+  }
 }
